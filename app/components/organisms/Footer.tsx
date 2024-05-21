755d838390760602137dde8769b6ecb4d6e2c99e
Archit <archit5793@gmail.com>
2024-05-20 23:55:11 -0400

    fix footer

--- app/components/organisms/Footer.tsx
+++ app/components/organisms/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className="text-neutral-400 dark:text-neutral-400 text-center px-2 mt-4">
+		<footer className="text-neutral-400 dark:text-neutral-400 text-center px-2 mt-16 sm:mt-4">
 			<p>&copy; {new Date().getFullYear()} Archit. All rights reserved.</p>
 		</footer>
 	);
